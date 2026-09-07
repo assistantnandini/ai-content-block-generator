2d45ccad4a03a98c9cf054c3bc4d419c877c8977
Nandini Singh <nandanisingh3286@gmail.com>
2026-09-07 12:47:32 +0530

    Use deployment port

--- server/index.js
+++ server/index.js
@@ -817,7 +817,7 @@ function makeFeatureDescriptionBetter(
 // START SERVER
 // --------------------------------------------------
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
